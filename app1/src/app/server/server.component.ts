fec1f486763fb5535ebc4e5c86722216da7b2708
bpreki <prescott.byron@gmail.com>
2019-03-15 15:01:06 +0000

    inital angular proof of concept micro apps

---
+++ app1/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';    // import decorator
+
+@Component({                                  // impiment decorator
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
